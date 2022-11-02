7a4230abe0b4fb11e90e3bf5006a76690d46fe77
Sergey Nacharov <nacharovsa@it2g.ru>
2022-11-02 14:16:44 +0400

    swiper

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,38 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(() => {
+    $(window).on('load', function hidePreloader() {
+        $('.preloader__wrp').fadeOut();
+    });
+});
+
+$(() => {
+    const indSlider = new Swiper(".js-ind-slider", {
+        speed: 1000,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: false,
+        },
+        mousewheel: true,
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 24,
+            },
+            576: {
+                slidesPerView: 2,
+                spaceBetween: 24,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 24,
+            },
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 35,
+                scrollbar: {
+                    snapOnRelease: true,
+                    draggable: true,
+                },
+            },
+        }
+    });
 });
